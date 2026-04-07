3c3d19bb72a8541863ffb53c16363ea43af57b9a
VitorEduardo017 <vitorduducontato@gmail.com>
2026-04-07 03:49:22 -0300

    MedProfit v2.0 - React/Vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MedProfit from './MedProfit.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MedProfit />
+  </React.StrictMode>,
+)
